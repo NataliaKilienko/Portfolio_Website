c28cab821fcdc268891d85e63a280df8a15c2ffd
NataliaKilienko <kilienko900@gmail.com>
2024-08-06 16:40:23 +0300

    Ensure Home component loads correctly on initial render

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import "./styles/main.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Project from "./pages/Project";
-import Contacts from "./pages/Contacts";
-
-import ScrollToTop from "./utils/scrollToTop";
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Project from './pages/Project';
+import Contacts from './pages/Contacts';
+import ScrollToTop from './utils/scrollToTop';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/Portfolio-Website">
         <ScrollToTop />
         <Navbar />
         <Routes>
@@ -30,3 +29,4 @@ function App() {
 }
 
 export default App;
+
